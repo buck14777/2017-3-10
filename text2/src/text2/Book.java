b7beea0ccb2764d1045078146493f52428f3abed
buck14777 <105021048@live.asia.edu.tw>
2017-03-10 16:11:03 +0800

    2017-3-10    02

--- text2/src/text2/Book.java
+++ text2/src/text2/Book.java
@@ -2,5 +2,21 @@ package text2;
 
 	public class Book {
 		private String name, isbn;
-		
+	public Book(String name1,String isbn1){
+		name = name1;
+		isbn = isbn1;
+	}
+	public void setName(String name1){
+		name=name1;
+	}
+	public String getNane(){
+		return name;
+	}
+	public void setIsbn(String isbn1){
+		isbn=isbn1;
+	}
+	public String getIsbn(){
+		return isbn;
+	}
+
 	}
